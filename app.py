b1cfbe9e71f44135a48e3f115c717cfdc89fbfa6
Sohail Mohammed <sohailm.1999@gmail.com>
2021-09-05 23:42:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import warnings
 warnings.filterwarnings("ignore")
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import streamlit as st 
-st.title('Time Series Forecasting')
+st.title('Time Series Forecasting for Availability of Beds ')
 st.sidebar.header('User Input Parameters')
-Total_beds = st.sidebar.number_input("Insert the number of Total beds")
-Days = st.sidebar.number_input("Insert the number of days of prediction",min_value = 1, max_value = 365)
+Total_beds = st.sidebar.number_input("Enter the number of Total beds")
+Days = st.sidebar.number_input("Enter the number of days of prediction",min_value = 1, max_value = 365)
 df=pd.read_csv('Beds_Occupied.csv')
 for x in ['Total Inpatient Beds']:
     q75,q25 = np.percentile(df.loc[:,x],[75,25])
@@ -37,20 +37,11 @@ train = df4.iloc[0:280,:]
 test = df4.iloc[280:,:]
 hwe_model_add_sea = ExponentialSmoothing(train["Available_beds"],seasonal="add",seasonal_periods=12).fit() #add the trend to the model
 pred_hwe_add_sea_test = hwe_model_add_sea.predict(start = test.index[0],end = test.index[-1])
-def main():
-    st.title('Time Series Forecasting')
-    html_temp = """
-    <div style="background-color:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">Time Series Forecasting for the Availabilty of beds' </h2>
-    </div>
-    """
-    st.markdown(html_temp, unsafe_allow_html=True)
-
 input=Days
 prediction=hwe_model_add_sea.forecast(input)
 st.subheader('Predicted Result')
 st.write(prediction)
-st.subheader("VISUALIZE FORECASTED DATA")
+st.subheader("Visualization of the Prediction")
 st.line_chart(df4['Available_beds'])
 st.line_chart(prediction)
 plt.plot(df4['Available_beds'], label='original')
